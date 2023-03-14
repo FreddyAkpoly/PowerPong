5298e226250d59289c8ed804cb7648bedeca96f5
Freddy Akpoly <fakpoly@u.rochester.edu>
2023-03-14 02:17:33 -0400

    music loop

---
+++ Pong3DUpdated/Assets/MusicLoop.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MusicLoop : MonoBehaviour
+{
+    public AudioSource musicSource;
+    public AudioClip musicStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicSource.PlayOneShot(musicStart);
+        musicSource.PlayScheduled((AudioSettings.dspTime + musicStart.length)-2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        
+    }
+}
